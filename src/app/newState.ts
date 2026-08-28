96f7fe2d7ecaee9943602e4ae0f9ee989dd2a97f
5argan <tizian.hoffmann@gmail.com>
2026-08-29 01:02:42 +0200

    Add state struct

--- src/app/newState.ts
+++ src/app/newState.ts
@@ -1,5 +1,25 @@
-export type AppState = {};
+import { Material } from '../api-access';
+
+type TabId = 'crafting-tree' | 'settings';
+type Settings = { apiKey: string | undefined };
+
+// TODO: move
+type MaterialId = string;
+type CraftingTreeState = {
+  searchText: string | undefined;
+  extractionYields: Record<MaterialId, number>;
+  targetMaterial: Material | undefined;
+};
+export type AppState = {
+  selectedTab: TabId;
+  settings: Settings;
+  craftingTree: CraftingTreeState;
+};
 
 export function createInitialState(): AppState {
-  return {};
+  return {
+    selectedTab: 'crafting-tree',
+    settings: { apiKey: undefined },
+    craftingTree: { searchText: undefined, extractionYields: {}, targetMaterial: undefined },
+  };
 }
