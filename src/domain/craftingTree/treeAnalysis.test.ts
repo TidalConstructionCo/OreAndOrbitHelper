09c811928005aeeb25029ca423561f5195d1a3c2
5argan <tizian.hoffmann@gmail.com>
2026-08-31 01:09:03 +0200

    Add another test

--- src/domain/craftingTree/treeAnalysis.test.ts
+++ src/domain/craftingTree/treeAnalysis.test.ts
@@ -37,4 +37,40 @@ describe('getSummedUtilization', () => {
     console.log(JSON.stringify(tree));
     expect(actual).toEqual(expected);
   });
+  it('calculates utilization for multiple levels', () => {
+    // arrange
+    const targetMaterial = createDummyMaterial({ id: 'targetId' });
+    const inputMaterial = createDummyMaterial({ id: 'inputId' });
+    const inputMaterial2 = createDummyMaterial({ id: 'inputId2' });
+    const rawMaterial = createDummyMaterial({ id: 'rawId' });
+    const inputMaterialRecipe2 = createDummyRecipe({
+      duration: 1,
+      inputs: [{ material: rawMaterial, qty: 1 }],
+      output: { material: inputMaterial2, qty: 3 },
+    });
+    const inputMaterialRecipe = createDummyRecipe({
+      duration: 1,
+      inputs: [{ material: inputMaterial2, qty: 1 }],
+      output: { material: inputMaterial, qty: 2 },
+    });
+    const mainRecipe = createDummyRecipe({
+      duration: 2,
+      inputs: [{ material: inputMaterial, qty: 1 }],
+      output: { material: targetMaterial, qty: 1 },
+    });
+    const tree = buildTree(
+      targetMaterial,
+      [targetMaterial, inputMaterial, inputMaterial2, rawMaterial],
+      [inputMaterialRecipe, inputMaterialRecipe2, mainRecipe],
+      new Map(),
+    );
+
+    // act
+    const actual = getSummedUtilization(tree);
+
+    // assert
+    expect(actual.get(mainRecipe)).toEqual(1);
+    expect(actual.get(inputMaterialRecipe)).toEqual(1 / 4);
+    expect(actual.get(inputMaterialRecipe2)).toEqual(1 / 6);
+  });
 });
